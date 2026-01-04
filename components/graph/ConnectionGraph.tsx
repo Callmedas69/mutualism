dd31ce578bc4ac5fc02c8af40bb3a84802c92ecf
Herryanto <phan.harry@gmail.com>
2026-01-04 23:07:04 +0700

    disable token button

--- components/graph/ConnectionGraph.tsx
+++ components/graph/ConnectionGraph.tsx
@@ -639,11 +639,11 @@ function ConnectionGraph({ connections, centerUser, type }: ConnectionGraphProps
                 onExport={handleExportPNG}
                 disabled={isEngineRunning}
               />
-              <TokenizeButton
+{/* <TokenizeButton
                 getGraphBlob={getGraphBlob}
                 graphData={tokenizeData}
                 disabled={isEngineRunning}
-              />
+              /> */}
             </>
           )}
         </div>
